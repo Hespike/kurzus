2d9258ba7e19688896f03e0291b0845e901ecf0f
Sárdi András <sardi.andras@gmail.com>
2021-11-19 11:53:56 +0100

    Create kurzus.cpp

---
+++ kurzus.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void kurzus_meghirdetes(string nev, int ferohelyek = 20) {
+    cout << "A(z) " << nev << " kurzus " << ferohelyek << " fore megnyitasra kerult.";
+}
+
